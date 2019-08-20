0067fbd0daf7cd262707300b68f6510eb0fd05d8
HansamalDharmananda <hansamal2010@gmail.com>
2019-08-20 21:55:24 +0530

    #6 Create User routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const connection = mongoose.connect('mongodb+srv://hansamal:hansamal1234@cluster
 connection .then((db) => { console.log("Connected correctly to server"); }) .catch((err) => { console.log(err) });
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 
 var app = express();
 
